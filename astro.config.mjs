c61fcb21cf380e21e13d004baec189bd5097e00c
BuschFranco <yochicos@gmail.com>
2024-07-11 01:25:29 -0300

    aj

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://BuschFranco.github.io',
+    site: 'https://buschfranco.github.io',
     base: '/portafolio',
 });
