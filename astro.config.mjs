8114a5a581b499d630c92dd357f65ee8faadc0d4
BuschFranco <yochicos@gmail.com>
2024-07-12 14:47:39 -0300

    ajuste

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,8 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/serverless';
-
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://www.buschfranco.com',
-    integrations: [sitemap()],
-
-    output: 'server',
-    adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
+    integrations: [sitemap()]
 });
