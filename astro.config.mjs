e25b67fbdcafa10e3c3095bcb0447fa43effce52
BuschFranco <yochicos@gmail.com>
2024-07-12 14:33:28 -0300

    agregando web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless';
+
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://www.buschfranco.com',
-    integrations: [sitemap()]
+    integrations: [sitemap()],
+
+    output: 'server',
+    adapter: vercel({
+    webAnalytics: { enabled: true }
+  })
 });
