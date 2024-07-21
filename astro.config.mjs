9e334814b995fb1156f556e7382b82ed20134a28
BuschFranco <yochicos@gmail.com>
2024-07-21 17:47:47 -0300

    agregando inglés

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-
 import partytown from "@astrojs/partytown";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.buschfranco.com',
-  integrations: [sitemap(), partytown()],
-
+  integrations: [sitemap(), partytown(), mdx()],
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en"],
