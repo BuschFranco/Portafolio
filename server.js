32baee5e25550c3bfaf067a2f2ebf24c2c01a5c5
BuschFranco <francobusch130@gmail.com>
2024-08-27 02:55:56 -0300

    integrando backend (desactivado)

--- server.js
+++ server.js
@@ -2,32 +2,13 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
-
 app.use(cors());
+app.use(express.json());
 
-// Redirigir directamente a Google Tag Manager
-app.get('/gtag/js', (req, res) => {
-  res.redirect('https://www.googletagmanager.com/gtag/js?id=AW-16671908962');
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from Express API!' });
 });
 
-// Configurar la ruta para servir el script de Google Ad
-app.get('/', (req, res) => {
-  res.send(`
-    <div>
-      <!-- Google tag (gtag.js) -->
-      <script async src="/gtag/js"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'AW-16671908962');
-      </script>
-    </div>
-  `);
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+export default (req, res) => {
+  app(req, res);
+};
